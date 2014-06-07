348f6aa11b2669103dcda81550c22b2bd88374d6
Jare Guo <jare85@163.com>
2014-06-07 16:32:24 +0800

    init

---
+++ src/get_font_list.js
@@ -0,0 +1,96 @@
+﻿var getFontList = (function () {
+
+    var Path = require('path');
+    var Fs = require('fs');
+    //var Fontpath = require('fontpath');
+    var FontParser = require('./font_parser');
+
+    var FONT_DIRS = {
+        win32: '/Windows/fonts',
+        darwin: '/Library/Fonts',
+        linux: '/usr/share/fonts/truetype'
+    };
+    var fontDir = Path.resolve(FONT_DIRS[process.platform]);
+    //var fontDir = "C:\\Users\\123\\Desktop\\testFonts\\";
+
+    // I have not tried each format, if not support Freetype will crash. In order to prevent this, just disable some of them first.
+    var SUPPORT_FORMATS = ['ttf', 'ttc', 'otf', 'dfont'];   // ['pfa', 'pfb', 'cff', 'otc', 'bdf', 'pfr'];
+
+    var getFontFamily = function (fonts, language) {
+
+        var retval = [];
+        var dir = fontDir + Path.sep;
+
+        var getFontInfo = function (fontInfo) {
+            var name = fontInfo.name;
+            var index = binaryIndexOf.call(retval, name);
+            if (index < 0) {
+                //console.log(name);
+                //console.dir(fontInfo);
+                retval.splice(~index, 0, name);
+            }
+        };
+        for (var i = 0, len = fonts.length; i < len; i++) {
+            var ext = Path.extname(fonts[i]).slice(1).toLowerCase();
+            if (SUPPORT_FORMATS.indexOf(ext) === -1) {
+                continue;
+            }
+            var filePath = dir + fonts[i];
+            var buffer = Fs.readFileSync(filePath);
+            var fontInfo = { name: FontParser.getFontName(buffer, language) };
+            getFontInfo(fontInfo);
+        }
+        return retval;
+    }
+
+    return function (callback, language) {
+        Fs.readdir(fontDir, function (err, files) {
+            if (!err) {
+                var fontFamilys = getFontFamily(files, language);
+                callback(fontFamilys);
+            }
+            else {
+                console.error(err);
+            }
+        });
+    };
+})();
+
+/**
+ * Modified from http://oli.me.uk/2013/06/08/searching-javascript-arrays-with-a-binary-search/
+ * Performs a binary search on the host array. This method can either be
+ * injected into Array.prototype or called with a specified scope like this:
+ * - binaryIndexOf.call(someArray, searchElement);
+ * @param {*} searchElement The item to search for within the sorted array.
+ * @return {Number} The zero-based index of item in the array, if item is found; otherwise,
+ *                  a negative number that is the bitwise complement of the index of
+ *                  the next element that is larger than item or, if there is no larger element,
+ *                  the bitwise complement of Count.
+ */
+function binaryIndexOf(searchElement) {
+    'use strict';
+
+    var minIndex = 0;
+    var maxIndex = this.length - 1;
+    var currentIndex;
+    var currentElement;
+
+    while (minIndex <= maxIndex) {
+        currentIndex = (minIndex + maxIndex) / 2 | 0;
+        currentElement = this[currentIndex];
+
+        if (currentElement < searchElement) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > searchElement) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+
+    return ~minIndex;
+}
+
+module.exports = getFontList;
